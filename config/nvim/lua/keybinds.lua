ab7d48cf3dccf07523de24af0c40c35c7aa095c9
Christian Howe <cj@cjhowe.dev>
2021-10-24 16:39:32 -0700

    Convert some of the config to lua

--- config/nvim/lua/keybinds.lua
+++ config/nvim/lua/keybinds.lua
@@ -1,10 +1,1 @@
-vim.cmd([[
-nnoremap <Space> <Nop>
-let mapleader = " "
-
-nnoremap <silent> <leader>f :FZF<cr>
-nnoremap <silent> <leader>q :bp\|bd #<cr>
-
-inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-  \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-]])
+vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {})
