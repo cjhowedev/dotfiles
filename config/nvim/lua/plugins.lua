3f8c75769f4e3d6297b750091c86ce4c23fcb3a6
Christian Howe <cj@cjhowe.dev>
2022-04-04 07:44:20 -0700

    Configure nvim-autopairs

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -67,6 +67,18 @@ return require('packer').startup(function(use)
             }
         end
     }
+    use {
+        'windwp/nvim-autopairs',
+        requires = {'hrsh7th/nvim-cmp'},
+        config = function()
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on('confirm_done',
+                         cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+
+            require'nvim-autopairs'.setup {ignored_next_char = "[%w%.]"}
+        end
+    }
 
     -- Project Support
 
